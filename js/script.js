17aa329c3ce8ce980a63053f1561363937cce1e9
Naomi Gibson <128326374+NaomiGibson@users.noreply.github.com>
2023-11-07 10:32:25 +0000

    Snakes Spawn Randomly test

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 var interval;
 var myGameArea;
-var isInPlay;
-var upKeyPressed;
-var downKeyPressed;
+var isInPlay = false;
+var upKeyPressed = false;
+var downKeyPressed = false;
+var frame = 0;
 
 document.addEventListener('keydown', (e) => {
     if (e.code === "ArrowUp") {
@@ -23,28 +24,28 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
 function startGame() {
-    noodle = new Snake(-50, 210, 3, 50);
-    noodle1 = new Snake(-110, 69, 2, 50);
-    noodle2 = new Snake(-350, 400, 3, 50);
-    noodle3 = new Snake(-390, 320, 2, 50);
-    noodle4 = new Snake(-470, 425, 4, 50);
+    //noodle = new Snake(-50, 210, 3, 50);
+    //noodle1 = new Snake(-110, 69, 2, 50);
+    //noodle2 = new Snake(-350, 400, 3, 50);
+    //noodle3 = new Snake(-390, 320, 2, 50);
+    //noodle4 = new Snake(-470, 425, 4, 50);
     myGameArea = new GameArea();
     playerOne = new Player(700, 200, "#874356");
     isInPlay = true;
     interval = setInterval(updateGame, 10);
-    myGameArea.components.push(playerOne, noodle, noodle1, noodle2, noodle3, noodle4);
+    myGameArea.components.push(playerOne); //, noodle, noodle1, noodle2, noodle3, noodle4
     myGameArea.start();
 }
-function updateGame() {
-    if (isInPlay) {
-        myGameArea.update();
-    }
-}
-function stopGame() {
-    clearInterval(interval);
-    myGameArea.stop();
-    isInPlay = false;
+function spawnNewSnake() {
+    var y = getRandomInt(15, myGameArea.canvas.height - 15);
+    var speed = 2;
+    var length = 50;
+    myGameArea.components.push(new Snake(-length, y, speed, length));
 }
 function stopComponent(component) {
     component.isInPlay = false;
@@ -56,5 +57,23 @@ function loseGame() {
 function winGame() {
     myGameArea.winGame;
 }
+function stopGame() {
+    clearInterval(interval);
+    myGameArea.stop();
+    isInPlay = false;
+}
+function updateGame() {
+    if (isInPlay) {
+        var nextSpawnFrame = 0
+        if (frame > nextSpawnFrame) {
+            spawnNewSnake();
+            nextSpawnFrame += getRandomInt(1, 11);
+        }
+        myGameArea.update();
+        frame ++;
+    }
+}
+
+
 
 startGame();
